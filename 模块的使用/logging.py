ebe9407baf3becd64db1822d92443786b8f4d2b0
dzwern <1048938804@qq.com>
2021-09-20 21:53:19 +0800

    模型

---
+++ 模块的使用/logging.py
@@ -0,0 +1,24 @@
+import logging
+
+# 日志的级别
+
+logging.debug('debug')
+logging.info('info')
+logging.warning('warning')
+logging.error('error')
+logging.critical('critical')
+
+# 上述，只会打印出来三个,其余默认不显示
+
+# 打印日志的级别，critical>error>warning>info>debug
+
+
+
+
+
+
+
+
+
+
+
